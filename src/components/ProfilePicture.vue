17579d4e900b0d41045b7f2f85c38c98899de8fc
Hampus Rönström <h_ronstrom@hotmail.com>
2019-12-13 03:03:49 +0100

    Adds dummy profile pic for animals

---
+++ src/components/ProfilePicture.vue
@@ -0,0 +1,14 @@
+<template>
+  <span class="profile-picture" :style="{color: color, 'float':'right'}">
+      <i class="fas fa-otter fa-4x"></i>
+  </span>
+</template>
+
+<script>
+export default {
+    name: 'profilePicture',
+    props: {
+        color: String
+    }
+}
+</script>
