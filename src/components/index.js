57f5318ad283c182ef71cf4640911196c3bb9540
Hampus Rönström <h_ronstrom@hotmail.com>
2019-12-13 01:58:58 +0100

    Adds modal

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,14 @@ import FVButton from "./Button.vue";
 import FVIconButton from "./IconButton.vue";
 import FVPanel from "./Panel.vue";
 import FVInputGroup from "./InputGroup.vue";
+import FVModal from "./Modal.vue";
 
 const Components = {
   FVButton,
   FVIconButton,
   FVPanel,
-  FVInputGroup
+  FVInputGroup,
+  FVModal
 };
 
 Object.keys(Components).forEach(name => {
